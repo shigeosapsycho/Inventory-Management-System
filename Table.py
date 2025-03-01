999709e9a1455c4b24aed778af6407d5af73de88
shigeosapsycho <brandon.tran01@student.csulb.edu>
2025-03-01 00:46:39 -0800

    drafting all of this before i commit to a huge change in main.py

---
+++ Table.py
@@ -0,0 +1,16 @@
+class Table:
+    def __init__():
+        pass
+    
+    def add_row(set, item, price, range1, range2):
+        """ 
+        Add row template:
+| {set} | ###################################### |    {stock}    |   {price}    |  {range1}-{range2} | # We need to figure out how to do items still
++-----+----------------------------------------+----------+----------+----------+  
+        """
+        if len(set) >= 3:
+            pass # Make it ask again but I have not implemented that yet. 
+        if len(item) >= 40:
+            pass # Make it 
+        pass
+
--- main.py
+++ main.py
@@ -36,32 +36,44 @@ def make_file():
         with open(sold_file_path, "w") as file:
             pass
 
+def display_options(num):
+    match num:
+        case 1:
+            print("1. Check Inventory\n2. Add Item\n3. Remove Item\n4. Update Item\n5. Clear Inventory")
+            print("")
+        case 2:
+            print("1. Add note\n2. Check notes\n3. Terminate program")
+
 def main(): # Menu
     while True:
         # Precondiiton and check text file
         make_file()
+        print("Welcome to the Inventory Management System!", end="")
+        display_options(num=1)
         
-        option = int(input("Welcome to the Inventory Management System!\n1. Check Inventory\n2. Add Item\n3. Remove Item\n4. Update Item Stock\n5. Clear Inventory\n6.Check Items Sold\n7. Exit\n\n"))
+        option = input()
+        # option = input("Welcome to the Inventory Management System!\n1. Check Inventory\n2. Add Item\n3. Remove Item\n4. Update Item\n5. Clear Inventory\n6. Check Items Sold\n7. Exit\n\n") # Currently this is a lot of options I might do a page thing
+        
         match option:
-            case 1:
+            case "1":
                 print("Option 1")
                 break
-            case 2:
+            case "2":
                 break
-            case 3:
+            case "3":
                 break
-            case 4:
+            case "4":
                 break
-            case 5:
+            case "5":
                 break
-            case 6:
+            case "6":
                 break
-            case 7:
+            case "7":
                 print("Goodbye. All your progress has been saved")
                 break
             case _:
-                print("Invalid option.")
-                break
+                print("Invalid option.\n")
+                # break
             
 if __name__ == "__main__":
     main()
